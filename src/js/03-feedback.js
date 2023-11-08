55051596eb7f23cbf76e9b8ff86976a83f641822
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-11-08 11:26:03 +0200

    added solutions

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import localStorageAPI from './localstorage.js';
 
+const contactFormEl = document.querySelector('.feedback-form');
+const userData = {};
+
+const fillContactFormField = () => {
+  const userDataFromLS = localStorageAPI.load('contactFormInfo');
+
+  if (userDataFromLS === undefined) {
+    return;
+  }
+  for (const key in userDataFromLS) {
+    if (userDataFromLS.hasOwnProperty(key)) {
+      contactFormEl.elements[key].value = userDataFromLS[key];
+    }
+  }
+};
+
+fillContactFormField();
+
+const onContactFormFieldChange = ({ target: contactFormField }) => {
+  const contactFormFieldValue = contactFormField.value;
+  const contactFormFieldName = contactFormField.name;
+
+  userData[contactFormFieldName] = contactFormFieldValue;
+
+  localStorageAPI.save('contactFormInfo', userData);
+};
+
+const onContactFormSubmit = event => {
+  event.preventDefault();
+
+  contactFormEl.reset();
+  localStorageAPI.remove('contactFormInfo');
+};
+
+contactFormEl.addEventListener('change', onContactFormFieldChange);
+contactFormEl.addEventListener('submit', onContactFormSubmit);
